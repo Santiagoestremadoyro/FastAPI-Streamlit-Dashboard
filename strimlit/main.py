c0092e8efcf4bbd3a7940980498f6e3e84533f16
santiago <estremadoyros@gmail.com>
2023-02-23 21:50:06 +0100

    gets

--- strimlit/main.py
+++ strimlit/main.py
@@ -1,7 +1,22 @@
 import streamlit as st
+from get import get_sex
+from router.pinguins import get_pinguins, get_pinguins_i, get_pinguins_s
 
 st.markdown("<h1 style='text-decoration: underline;'>Palmer Archipelago (Antarctica)</h1>", unsafe_allow_html=True)
 
-st.image('./pinguinos.jpg')
+st.image('./data/pinguinos.jpg')
 
-st.selectbox("What would you like to see?", ["by sex", "by islands", "by species"])
+selectbox = st.selectbox("What would you like to see?", ["by sex", "by islands", "by species"])
+
+
+
+
+if selectbox == "by sex":
+    data = get_sex
+    create_bar_chart(data, "sex", "count")
+elif selectbox == "by islands":
+    data = get_pinguins_i
+    create_bar_chart(data, "island", "count")
+elif selectbox == "by species":
+    data = get_pinguins_s
+    create_bar_chart(data, "species", "count")
